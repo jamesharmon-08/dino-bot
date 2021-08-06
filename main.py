06c1b72bf2ca03772a50e92a54ede3714871c18a
jamesharmon <james.harmon@outlook.com>
2021-08-05 20:55:58 -0400

    tweak to collision range

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from mss import mss
 from pynput.keyboard import Key, Controller
 import time
 
-monitor = {"top": 430, "left": 500, "width": 1, "height": 32,}
-output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
+monitor = {"top": 430, "left": 460, "width": 16, "height": 32,}
+
 keyboard = Controller()
 running = True
 ctr=0
@@ -18,7 +18,7 @@ while running:
         sct_img = sct.grab(monitor)
         img = Image.frombytes("RGB", (sct_img.size), sct_img.rgb).convert("L")
     pixels = list(img.getdata())
-    print(f'{pixels[24]} | {ctr}')
+
     for pixel in pixels:
         if pixel < 100:
             keyboard.press(Key.up)
